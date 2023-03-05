c76b306d2c5c5db08e9f2d41812f781da0a9b6e9
mahmudul165 <mahmudulh883@gmail.com>
2023-03-06 04:29:13 +0600

    comment out superadmin to app

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors"); 
- const app = express();
- // Enable CORS for all origins
- app.use(cors());
+// const express = require("express");
+// const cors = require("cors"); 
+//  const app = express();
+//  // Enable CORS for all origins
+//  app.use(cors());
 
  
 // start
