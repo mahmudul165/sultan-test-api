d8a599bea6c3da3875073e531b47446658ecbb8a
mahmudul165 <mahmudulh883@gmail.com>
2023-03-19 15:38:21 +0600

    added

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -6,7 +6,7 @@ const rateLimiter = rateLimit(
     {  
         
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 300, // limit each IP to 100 requests per windowMs
 });
 
 module.exports = rateLimiter;
