c8a190e01f5bd3110001dc3e9d9db943a3ff9a5a
mahmudul165 <mahmudulh883@gmail.com>
2023-03-24 23:26:59 +0600

    added json parse

--- middleware/cacheMiddleware.js
+++ middleware/cacheMiddleware.js
@@ -23,7 +23,7 @@
 
 const NodeCache = require('node-cache');
 
-const cache = new NodeCache({ stdTTL: 6, checkperiod: 240 });
+const cache = new NodeCache({ stdTTL: 30, checkperiod: 240 });
 
 const cacheMiddleware = (req, res, next) => {
   try {
