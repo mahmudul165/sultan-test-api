3a475fff1d8d0409ec9ee557c827b5fbc43f3d28
Mahmudul Hasan <61279339+mahmudul165@users.noreply.github.com>
2024-12-15 14:25:54 +0600

    Update db.js
    mongo connection

--- config/db.js
+++ config/db.js
@@ -1,13 +1,32 @@
+// const mongoose = require("mongoose");
+// const config = require("./config");
+// const dbURL = config.db.url;
+
+// mongoose
+//   .connect(dbURL)
+//   .then(() => {
+//     console.log("mongodb atlas is connected");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//     process.exit(1);
+//   });
 const mongoose = require("mongoose");
 const config = require("./config");
 const dbURL = config.db.url;
 
+// Set 'strictQuery' to false or true based on your preference to avoid deprecation warning
+mongoose.set('strictQuery', false); // You can also set it to true if needed
+
 mongoose
-  .connect(dbURL)
+  .connect(dbURL, {
+    useNewUrlParser: true, // Enable the new MongoDB parser
+    useUnifiedTopology: true, // Use the unified topology engine
+  })
   .then(() => {
-    console.log("mongodb atlas is connected");
+    console.log("MongoDB Atlas is connected");
   })
   .catch((error) => {
-    console.log(error);
-    process.exit(1);
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process in case of an error
   });
