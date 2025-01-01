50ba308d717abf71d38b1cf3e75f0661067b35da
Mahmudul Hasan <61279339+mahmudul165@users.noreply.github.com>
2025-01-01 19:08:03 +0600

    just add in index.js file

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const config = require("./config/config");
 
 const PORT = config.app.port;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`app is running on http://localhost:${PORT}`);
+// });
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`app is running on http://localhost:${PORT}`);
 });
